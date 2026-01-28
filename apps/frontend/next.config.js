62dcff2895ab04fc79314f7123ed90c96e67c0f8
Nudesuppe42 <67996941+Nudelsuppe42@users.noreply.github.com>
2026-01-28 21:16:07 +0100

    feat(frontend/conf): Add raw githubcontent url to allowed image patterns

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -28,6 +28,12 @@ const nextConfig = {
 				port: '',
 				pathname: '/**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'raw.githubusercontent.com',
+				port: '',
+				pathname: '/**',
+			},
 		],
 	},
 	i18n: {
