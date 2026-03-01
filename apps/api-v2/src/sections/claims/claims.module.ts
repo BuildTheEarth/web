b02e641c855c3246e3c16c418cd5373b89ac9474
Nudesuppe42 <jufo.theo@gmail.com>
2026-03-01 19:57:48 +0100

    fix(api/claims): :bug: Add prisma service to claims module

--- apps/api-v2/src/sections/claims/claims.module.ts
+++ apps/api-v2/src/sections/claims/claims.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ClaimsService } from './claims.service';
-import { ClaimsController } from './claims.controller';
+import { Module } from "@nestjs/common";
+import { PrismaService } from "src/common/db/prisma.service";
+import { ClaimsController } from "./claims.controller";
+import { ClaimsService } from "./claims.service";
 
 @Module({
   controllers: [ClaimsController],
-  providers: [ClaimsService],
+  providers: [ClaimsService, PrismaService],
 })
 export class ClaimsModule {}
