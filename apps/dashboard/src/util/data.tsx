7180f981934a90cb20ce56f05ce0a6b9b1536fec
Nudesuppe42 <jufo.theo@gmail.com>
2026-09-20 15:48:00 +0200

    fix(dash/src): 🚑️ Change revalidation of website to actual api call used

--- apps/dashboard/src/util/data.tsx
+++ apps/dashboard/src/util/data.tsx
@@ -1,23 +1,17 @@
-export async function globalFetcher<T = {}>(route: string, ...props: any): Promise<T> {
-	const res = await fetch(route, ...props)
-	return res.json()
-}
-
-// Specific functions
-
 export function revalidateWebsitePath(path: string) {
 	return revalidateWebsitePaths([path])
 }
 export function revalidateWebsitePaths(paths: string[]) {
-	return globalFetcher(
-		`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/revalidate?secret=${process.env.FRONTEND_KEY}&paths=${JSON.stringify(
-			paths,
-		)}`,
-		{
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
+	return fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/revalidate?secret=${process.env.FRONTEND_KEY}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
 		},
-	)
+		body: JSON.stringify({ paths }),
+	}).then((res) => {
+		if (!res.ok) {
+			throw new Error(`Failed to revalidate paths: ${res.statusText}`)
+		}
+		return res.json()
+	})
 }
